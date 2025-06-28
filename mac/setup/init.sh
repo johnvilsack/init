8e3373d66e080b8ed1d62128bf4402dd5a431f42
John Vilsack <vilsack@gmail.com>
2025-06-27 21:18:10 -0500

    cleanup

--- mac/setup/init.sh
+++ mac/setup/init.sh
@@ -2,33 +2,35 @@
 
 set -euo pipefail
 
+echo "*** SETUP MAC ***"
+
 # Check for file that prevents installation
-if [[ -f "$HOME/.config/init.done" ]]
-then
-  INIT_NO_INSTALL="$(cat "$HOME/.config/init.done" 2>/dev/null)"
-  if [[ -n "${INIT_NO_INSTALL}" ]]
-  then
-    abort "Init cannot be installed because ${INIT_NO_INSTALL}."
-  else
-    abort "Init cannot be installed because $HOME/.config/init.done exists!"
-  fi
-fi
+# if [[ -f "$HOME/.config/init.done" ]]
+# then
+#   INIT_NO_INSTALL="$(cat "$HOME/.config/init.done" 2>/dev/null)"
+#   if [[ -n "${INIT_NO_INSTALL}" ]]
+#   then
+#     abort "Init cannot be installed because ${INIT_NO_INSTALL}."
+#   else
+#     abort "Init cannot be installed because $HOME/.config/init.done exists!"
+#   fi
+# fi
 
 # Install Homebrew
 if ! command -v brew >/dev/null 2>&1; then
-  echo "Installing Homebrew"
+  echo "[INSTALLING] Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Rosetta is required for Apple Silicon Macs to run x86_64 binaries
 if [[ "$(uname -m)" == "arm64" ]] && ! arch -x86_64 /usr/bin/true 2>/dev/null; then
-    echo "Rosetta is not installed. Installing Rosetta..."
+    echo "[INSTALLING] Rosetta"
     sudo /usr/sbin/softwareupdate --install-rosetta --agree-to-license 2>/dev/null
 fi
 
 # Github CLI
 if ! command -v gh >/dev/null 2>&1; then
-  echo "Installing GitHub CLI..."
+  echo "[INSTALLING] GitHub CLI"
   brew install gh
 fi
 
@@ -36,25 +38,22 @@ fi
 #gh auth status --hostname github.com &>/dev/null || echo "Logging in to Github..." && gh auth login --hostname github.com --git-protocol https --web
 if ! gh auth status --hostname github.com &>/dev/null; then
     #echo "Already logged in to GitHub"
-    echo "Not logged in to GitHub. Logging in..."
+    echo "[LOGIN] Authorizing GitHub"
     gh auth login --hostname github.com --git-protocol https --web
 fi
 
 
 # Clone dotfiles repo now that we're logged in
 if [ ! -d "$HOME/github/dotfiles" ]; then
-  echo "Cloning dotfiles repository..."
+  echo "[CLONING] dotfiles repository"
   mkdir -p "$(dirname "$HOME/github/dotfiles")"
   git clone "https://github.com/johnvilsack/dotfiles" "$HOME/github/dotfiles"
   chmod +x "$HOME/github/dotfiles/mac/install.sh"
 
 fi
 
-echo "Running dotfiles installation script..."
-/bin/bash "$HOME/github/dotfiles/mac/install.sh"
-
-
-
+echo "[RUNNING] dotfiles install script"
+exec /bin/bash "$HOME/github/dotfiles/mac/install.sh"
 
 
 
