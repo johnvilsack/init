581312c431638ebc7c696afa332ee9d826ece46c
John Vilsack <vilsack@gmail.com>
2025-07-07 13:34:32 -0500

    removed old script

--- mac/total-output.sh
+++
@@ -1,7 +0,0 @@
-{
-    echo "# ========== PART 1: init.sh =========="
-    cat ~/github/init/mac/setup/init.sh
-    echo ""
-    echo "# ========== PART 2: mac-install.sh =========="
-    cat "$DOTFILESPATH/mac-install.sh"
-} > ~/output.txt
