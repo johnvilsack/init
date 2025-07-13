8979657939b608e720210c1a241829d29d43f7a1
John Vilsack <vilsack@gmail.com>
2025-07-12 19:01:13 -0500

    Update init.sh

--- mac/setup/init.sh
+++ mac/setup/init.sh
@@ -8,6 +8,11 @@ export GITHUBPATH="$HOME/github"
 export DOTFILESPATH="$GITHUBPATH/dotfiles"
 export DOTFILESHOME="$DOTFILESPATH/$OS/files/HOME"
 
+# Temporary bypass to fix intune issues
+export HOMEBREW_BUNDLE_MAS_SKIP=1
+export HOMEBREW_FORBIDDEN_CASKS="google-chrome,microsoft-teams,the-unarchiver"
+export HOMEBREW_FORBIDDEN_FORMULAE="mas"
+
 # Logging
 RED='\033[0;31m'
 GREEN='\033[0;32m'
